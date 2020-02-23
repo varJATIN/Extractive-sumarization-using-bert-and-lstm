da73af488ed5c76c903dbf005feb4fdd09e830c9
JATIN VARSHNEY <48639782+varJATIN@users.noreply.github.com>
2020-02-23 16:09:37 +0530

    4

--- convert_to_ids.py
+++ convert_to_ids.py
@@ -2,7 +2,7 @@
 import load_news_and_tokenize
 
 #
-raw_news=raw_news.query('text_cnt<400 ')
+raw_news=raw_news.query('text_cnt<400 ') # to keep the final count to bert with input length<512
 raw_news=raw_news.reset_index()
 del raw_news['index']
 for i in raw_news['processed_text']:
