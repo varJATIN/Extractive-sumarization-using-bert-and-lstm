a1f9ffc6ab4ca57dda3735789082540f82e9db31
JATIN VARSHNEY <48639782+varJATIN@users.noreply.github.com>
2020-02-23 15:34:04 +0530

    Create convert_to_ids.py

---
+++ convert_to_ids.py
@@ -0,0 +1,12 @@
+#
+import load_news_and_tokenize
+
+#
+raw_news=raw_news.query('text_cnt<400 ')
+raw_news=raw_news.reset_index()
+del raw_news['index']
+for i in raw_news['processed_text']:
+  indexed_text.append(tokenizer.convert_tokens_to_ids(i))
+raw_news['indexed_text']=indexed_text
+
+
